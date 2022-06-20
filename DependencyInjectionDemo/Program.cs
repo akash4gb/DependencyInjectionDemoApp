526188cfc4194cb8d1349e0559882d560cc82170
Akash Khan <akash4gb@gmail.com>
2022-06-21 00:37:36 +0600

    3 types of DI in services.

--- DependencyInjectionDemo/Program.cs
+++ DependencyInjectionDemo/Program.cs
@@ -7,7 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddTransient<DemoLogic>();
+//Everytime you ask for a item's NEW instence, add AddTransient. Most common.
+//builder.Services.AddTransient<DemoLogic>();
+
+//Everytime you ask for a item's SAME instence EVERYWHARE, add AddSIngleton. most common in DesktopApp
+//builder.Services.AddSingleton<DemoLogic>();
+
+//Everytime you ask for a item's SAME instence For SAME person, add AddScoped. Most common in web
+builder.Services.AddScoped<DemoLogic>();
+
 
 var app = builder.Build();
 
