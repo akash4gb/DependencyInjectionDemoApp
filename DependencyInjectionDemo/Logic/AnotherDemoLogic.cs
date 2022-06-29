bc543a9f3f482019b51f724fd9a573f0aef4d435
Akash Khan <akash4gb@gmail.com>
2022-06-29 23:34:56 +0600

    Injeting logger in class

--- DependencyInjectionDemo/Logic/AnotherDemoLogic.cs
+++ DependencyInjectionDemo/Logic/AnotherDemoLogic.cs
@@ -2,14 +2,16 @@
 
 public class AnotherDemoLogic:IDemoLogic
 {
+    private readonly ILogger<AnotherDemoLogic> _logger;
+
     public int Value1 { get; private set; }
     public int Value2 { get; private set; }
 
-    public AnotherDemoLogic()
+    public AnotherDemoLogic(ILogger<AnotherDemoLogic> logger)
 
     {
         Value1 = Random.Shared.Next(999, 9999);
         Value2 = Random.Shared.Next(5000, 7000);
-
+        _logger = logger;
     }
 }
